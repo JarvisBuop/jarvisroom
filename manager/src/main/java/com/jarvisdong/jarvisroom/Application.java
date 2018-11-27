a1061c3b7920a6b22619e120c1151062390a6d23
mac <1961473915@qq.com>
2018-11-27 18:47:58 +0800

    all

--- manager/src/main/java/com/jarvisdong/jarvisroom/Application.java
+++ manager/src/main/java/com/jarvisdong/jarvisroom/Application.java
@@ -3,6 +3,8 @@ package com.jarvisdong.jarvisroom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,7 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaRepositories(basePackages = "com.jarvisdong.jarvisroom.repository")
-public class Application extends SpringBootServletInitializer {
+public class Application extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
 	
 	public Application() {
         super();
@@ -26,4 +28,9 @@ public class Application extends SpringBootServletInitializer {
 		SpringApplication.run(Application.class, args);
 	}
 
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		container.setPort(8082);
+	}
 }
