d08b8ea460f7b33c779499f5712c1dccca2b670f
JarvisDong <dongmm@handsure.com>
2018-12-05 09:07:53 +0800

    all

--- jarvisroom-manager-web/src/main/java/com/jarvisdong/manager/web/controller/BookController.java
+++ jarvisroom-manager-web/src/main/java/com/jarvisdong/manager/web/controller/BookController.java
@@ -1,13 +1,18 @@
 package com.jarvisdong.manager.web.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.jarvisdong.manager.web.utils.Constants;
 import com.jarvisdong.pojo.Book;
 import com.jarvisdong.service.BookService;
+import org.apache.http.protocol.HTTP;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -17,14 +22,26 @@ import java.util.List;
 @Controller
 @RequestMapping(value = "/book")
 public class BookController {
+    private static final Logger logger = LoggerFactory.getLogger(BookController.class);
+
     @Reference(version = "${demo.service.version}",
             application = "${dubbo.application.id}",
             url = "dubbo://localhost:20880")
     private BookService bookService;
 
-    @RequestMapping("/getAllBooks")
-    private ResponseEntity<Object> getAllBooks(){
-        List<Book> all = bookService.findAll();
-        return new ResponseEntity<Object>(all, HttpStatus.OK);
+    @RequestMapping(value = "/getAllBooks", method = {RequestMethod.GET, RequestMethod.POST})
+    public ResponseEntity<Object> getAllBooks(Model model) {
+        logger.info("查看APP页面权限接口开始");
+        try {
+            List<Book> all = bookService.findAll();
+            model.addAttribute("bookLists", all);
+//            return Constants.WebPageAddr.WEB_NAME_BOOK;
+//            return Constants.WebPageCommon.WEB_PAGE_NAME_ERROR;
+//            return "bookList";
+        } catch (Exception e) {
+            logger.info("查看APP页面权限接口出错:{}", e.getMessage());
+//            return Constants.WebPageCommon.WEB_PAGE_NAME_ERROR;
+        }
+        return new ResponseEntity<Object>("kkkk", HttpStatus.OK);
     }
 }
---
+++ jarvisroom-manager-web/src/main/java/com/jarvisdong/manager/web/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.jarvisdong.manager.web.utils;
+
+/**
+ * Created by JarvisDong on 2018/12/5.
+ */
+public class Constants {
+
+    public static class WebPageAddr{
+        public static final String WEB_NAME_BOOK="/book/bookList";
+    }
+
+    public static class WebPageCommon{
+        public static final String WEB_PAGE_NAME_ERROR="templates/common/error500";
+    }
+}
